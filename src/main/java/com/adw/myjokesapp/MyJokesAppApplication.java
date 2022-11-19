71ccfe7453f70232f0d66bc12c44cd5ad54aab4a
Neil Goldsmith <neilgoldsmith1218@gmail.com>
2022-11-19 13:58:37 -0500

    initial put

---
+++ src/main/java/com/adw/myjokesapp/MyJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.adw.myjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adw/myjokesapp/MyJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adw.myjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
